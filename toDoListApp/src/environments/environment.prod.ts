9d652cc13142a0e832425b02ba718c69a1913d05
RudolfKlikh <rudolfklikh@gmail.com>
2019-08-04 22:08:11 +0200

    Prod ToDoList

--- toDoListApp/src/environments/environment.prod.ts
+++ toDoListApp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBcXaHK4PAafSdigX1n3hbvqigGPXXV6Wg',
+    authDomain: 'todolist-de31b.firebaseapp.com',
+    databaseURL: 'https://todolist-de31b.firebaseio.com',
+    projectId: 'todolist-de31b',
+    storageBucket: 'todolist-de31b.appspot.com',
+    messagingSenderId: '303863117006', 
+    appId: '1:303863117006:web:cf3d94b8ef397c10'
+  }
 };
